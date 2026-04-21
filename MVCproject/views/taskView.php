acf114f318541bca00b87e5e9fa8fe29d09283fc
technofan900 <technofan900@gmail.com>
2026-04-21 14:00:42 +0300

    text in readme

---
+++ MVCproject/views/taskView.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task mini-project</title>
+</head>
+<body>
+    <p>ligma balss</p>
+</body>
+</html>
